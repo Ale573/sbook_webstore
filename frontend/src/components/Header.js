c68e0adc6fe9c58751b783ea8c5d0b5c70e21240
Alejandro Matos Mercado <alejandro,matos1@upr.edu>
2019-10-03 21:40:02 -0400

    Added link to home in logo

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 
 import Logo from '../images/book_icon.png';
 
@@ -6,12 +7,13 @@ export class Header extends Component {
     render() {
         return (
             <header className="main_header">
-                <img 
+                <Link to="/"><img 
                     className="logo" 
                     src={Logo} 
                     alt="Book Logo" 
                     height="130" 
                     width="150"/>
+                </Link>
                 <h1 className="header_title">S-Book Webstore</h1>
             </header>
         )
