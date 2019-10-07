b98e1a4d9a99c3840e06c70b547ff5a7239438af
cegr21 <cesargiti96@gmail.com>
2019-10-07 19:28:48 +0200

    working in dash

---
+++ frontend/src/components/Book.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+export class Book extends Component {constructor(props){
+    super(props)
+    this.state={
+        id: this.props[0],
+        name: this.props[1],
+        author: this.props[2],
+        year: this.props[3]
+    }
+}
+    render() {
+        return (
+            <div>
+                <h3>Book Profile</h3>
+                <ul>
+                    <li>Name: {this.state.name}</li>
+                    <li>Author: {this.state.author}</li>
+                    <li>Year: {this.state.year}</li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default Book
--- frontend/src/pages/Dash.js
+++ frontend/src/pages/Dash.js
@@ -1,10 +1,53 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import Cover from '../images/booktest.jpg'
+import Book from '../components/Book'
 
 export class Dash extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+            books:[
+                {
+                    id: '0',
+                    name:'Book 1',
+                    author: 'Author 1',
+                    year: '2000'
+                },
+                {
+                    id: '1',
+                    name:'Book 2',
+                    author: 'Author 2',
+                    year: '2001'
+                },
+                {
+                    id: '2',
+                    name:'Book 3',
+                    author: 'Author 3',
+                    year: '2002'
+                },
+                {
+                    id: '3',
+                    name:'Book 4',
+                    author: 'Author 4',
+                    year: '2003'
+                }
+            ]
+        }
+    }
     render() {
         return (
             <div>
-                <h1>Im in the Dash!</h1>
+                {this.state.books.map(book =>
+                   <Book
+                        {...book}
+                        key={book[id]}
+                        name={book[name]}
+                        author={book[author]}
+                        year={book[year]}>   
+                        </Book>
+                    )}
+                   
+                
             </div>
         )
     }
