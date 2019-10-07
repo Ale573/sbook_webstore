da0c91d1d32d135ab9c14e78bf430b476bc5eb54
Alejandro Matos Mercado <alejandro,matos1@upr.edu>
2019-10-07 18:59:36 -0400

    Added tests

--- frontend/src/tests/App.test.js
+++ frontend/src/tests/App.test.js
@@ -1,9 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from '../App';
+import Login from '../components/Login';
+import Register from '../components/Register';
+import Home from '../pages/Home';
+import Dash from '../pages/Dash';
+import {withRouter} from "react-router-dom";
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render((<Login />), div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(withRouter(<Register />), div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(withRouter(<Home />), div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(withRouter(<Dash />), div);
   ReactDOM.unmountComponentAtNode(div);
 });
