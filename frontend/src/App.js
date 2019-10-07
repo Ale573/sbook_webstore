b8530f92779e6d322b9466be06b535a61b95e207
Alejandro Matos Mercado <alejandro,matos1@upr.edu>
2019-10-07 18:02:24 -0400

    Added Protected Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import './styles/styles.css';
 
 import Home from './pages/Home';
 import Header from './components/Header';
 import Dash from './pages/Dash';
+import Auth from './components/Auth';
 
 function App() {
+
+  const PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route {...rest} render={(props) => (
+      Auth.isUserAuthenticated() 
+        ? <Component {...props} />
+        : <Redirect to='/' />
+    )} />
+  )
+
   return (
     <Router>
       <div className="app">
@@ -14,7 +24,7 @@ function App() {
             <Header />  
             <Switch>
               <Route path="/" exact component={Home}/>
-              <Route path="/dash" component={Dash}/>
+              <PrivateRoute path='/dash' component={Dash} />
             </Switch>
         </div>
       </div>
